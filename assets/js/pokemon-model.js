38ac0df582aaaae8dcea2daf76ce263a23a3290b
Paulo Rodrigo <paulorodrigoba@outlook.com>
2024-02-12 16:46:17 -0400

    atualização do pokemon

--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -5,4 +5,17 @@ class Pokemon {
     type;
     types = [];
     photo;
+    baseExp;
+    height;
+    weight;
+    abilities = [];
+    moves = [];
+    stats = [];
+    description;
+    generation;
+    eggGroups = [];
+    evolutionChain;
+    evolutions = [];
+    evolutionsSprites = [];
+    liked;
 }
